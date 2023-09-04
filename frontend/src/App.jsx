c61f0b55d0ea86f11fa64c58d7b8f3df0d6aaece
Analiadlt <delatorreanalia@gmail.com>
2023-09-04 17:55:25 -0300

    Router-HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
-function App() {
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">amazona</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductPage />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
